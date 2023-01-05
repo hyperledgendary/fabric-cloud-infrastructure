2baa0da245b968eca751a16509b681f18cafbe9c
Matthew B White <whitemat@uk.ibm.com>
2023-01-05 15:54:13 +0000

    Update action
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- fabric-ansible-action/entrypoint.sh
+++ fabric-ansible-action/entrypoint.sh
@@ -3,30 +3,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 set -xeuo pipefail
+export INPUT_PLAYBOOK=$1
+export ARGSFILE=${2:-none}
 
-# env | sort
-# ls -lart
-# ls -lart _cfg
+echo $INPUT_PLAYBOOK
+echo $ARGSFILE
 
-# # If this image is run with -u <random UID>, as happens on Red Hat OpenShift, then
-# # the user is not in the /etc/passwd file. This causes Ansible to fail, so we need
-# # to add the user to /etc/passwd now before Ansible runs.
-# if ! whoami &> /dev/null; then
-#     sed '/ibp-user/d' /etc/passwd > /tmp/passwd
-#     cat /tmp/passwd > /etc/passwd
-#     rm -f /tmp/passwd
-#     echo "ibp-user:x:$(id -u):0::/home/ibp-user:/bin/bash" >> /etc/passwd
-#     export HOME=/home/ibp-user
-# fi
 export PYTHONPATH=/home/ibp-user/.local/lib/python3.9/site-packages
 export HOME=/home/ibp-user
-cd ${HOME}
+# cd ${HOME}
 
 export KUBECONFIG="${GITHUB_WORKSPACE}/_cfg/k8s_context.yaml"
 
-# Run a shell or the specified command.
-if [ $# -eq 0 ]; then
-    exec /bin/bash
-else
+if [[ $ARGSFILE == "none" ]]; then
     ansible-playbook -e ROOT=${GITHUB_WORKSPACE} ${GITHUB_WORKSPACE}/${INPUT_PLAYBOOK}
+else
+    ansible-playbook -e ROOT=${GITHUB_WORKSPACE} --extra-vars "@${GITHUB_WORKSPACE}/${ARGSFILE}" ${GITHUB_WORKSPACE}/${INPUT_PLAYBOOK} -vvv
 fi
